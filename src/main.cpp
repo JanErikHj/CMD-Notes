26aec3139ae5645a35dceeb5a307ffc2d1a72feb
Jan Erik HJelseth <90834906+JanErikHj@users.noreply.github.com>
2024-10-13 17:20:14 +0200

    Added message and file write

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,46 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
+string GenerateMessage();
+void WriteToFile(string text);
+
 int main(int argc, char *argv[])
 {
-     int count;
+    string message;
+    if (argc > 1)
+    {
 
-    // Display each command-line argument.
-    cout << "\nCommand-line arguments:\n";
-    for( count = 0; count < argc; count++ )
-         cout << "  argv[" << count << "]   "
-                << argv[count] << "\n";
+        message = GenerateMessage();
+        WriteToFile(message);
+    }
     return 0;
+}
+
+// #####################################################################
+//                 Functions
+// #####################################################################
+
+string GenerateMessage()
+{
+    string messageType;
+    string message;
+    string dueDate;
+
+    messageType = __argv[1];
+    message = __argv[2];
+    dueDate = __argv[3];
+
+    return messageType + ": " + message + " Due: " + dueDate;
+}
+
+void WriteToFile(string text)
+{
+    ofstream file1;
+    file1.open("noteit.txt", ios::app);
+    file1.seekp(0, ios::end);
+
+    file1 << text << endl;
+    file1.close();
 }
